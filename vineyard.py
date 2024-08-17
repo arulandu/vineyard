015d5922e8eef4c1ccc45db28fffd7e1d957b2b3
Alvan Caleb Arulandu <calebarulandu@gmail.com>
2024-08-17 15:20:05 -0400

    linguist + bump

--- vineyard.py
+++ vineyard.py
@@ -11,27 +11,30 @@ import gudhi.wasserstein
 import gudhi.hera
 import ot
 
-def persistence(array, dimension=None):
-  height, width = array.shape
-  cubeComplex = gudhi.CubicalComplex(
-      dimensions = [width,height],
-      top_dimensional_cells = 255 - array.flatten()
-  )
- 
-  if dimension == None:
-    persistence = cubeComplex.persistence()
-  else:
-    cubeComplex.compute_persistence()
-    persistence = cubeComplex.persistence_intervals_in_dimension(dimension)
-    
-  return persistence
+INT_MAX = 2147483647
 
-def stitch(PDs, ts):
+def persistence(array, fmax=255., dimension=None):
+    height, width = array.shape
+    cubeComplex = gudhi.CubicalComplex(
+        dimensions = [width,height],
+        top_dimensional_cells = fmax - array.flatten()
+    )
+ 
+    if dimension == None:
+        persistence = cubeComplex.persistence()
+    else:
+        cubeComplex.compute_persistence()
+        persistence = cubeComplex.persistence_intervals_in_dimension(dimension)
+        persistence[np.isinf(persistence)] = fmax
+
+    return persistence
+
+def stitch(PDs, ts, method=gudhi.hera):
     vines = [[0, None, [x,]] for x in range(len(PDs[0]))]
     ends = {x:x for x in range(len(PDs[0]))}
     
     for i in range(1, len(ts)):
-        dist, match = gudhi.hera.wasserstein_distance(PDs[i-1], PDs[i], matching=True)
+        dist, match = method.wasserstein_distance(PDs[i-1], PDs[i], matching=True)
     
         baby = []
         # print("IIII", i)
@@ -65,11 +68,11 @@ def stitch(PDs, ts):
 
     return vines
 
-def vineyard(f, g, nt=100, verbose=False):
-    ts = np.linspace(0, 1, nt)
-    hs = np.array([t*f+(1-t)*g for t in ts]) 
-    PD0 = [persistence(h, dimension=1) for h in hs]
-    vines = stitch(PD0, ts)
+def make_pds(f, g, ts, fmax=255.0, dim=1):
+    return [persistence((1-t)*f+t*g, fmax=fmax, dimension=dim) for t in ts]
+
+def vineyard(ts, PD0, method=gudhi.hera, verbose=False):
+    vines = stitch(PD0, ts, method=method)
 
     poss = vines
     
@@ -87,62 +90,70 @@ def vineyard(f, g, nt=100, verbose=False):
         poss[i][2] = np.array(repl)
 
     res = [[[ts[p[0]+np.arange(len(p[2]))][i], *x] for i,x in enumerate(p[2])] for p in poss]
-    mx = np.max([np.max(p[2]) for p in poss if np.inf not in p[2]])
 
-    if verbose:
-        gudhi.plot_persistence_diagram(persistence(hs[len(ts)-1]))
-        plt.show()
-        gudhi.plot_persistence_diagram(persistence(hs[0]))
-        plt.show()
+    if verbose > 0:
+        bnds = [[np.max(p[2]), np.min(p[2])] for p in poss if np.inf not in p[2]]
+        mx, mn = (np.max([p[0] for p in bnds]), np.min([p[1] for p in bnds])) if len(bnds) > 0 else (INT_MAX, -INT_MAX)
+
+        if verbose == 1 or verbose == 3:
+            gudhi.plot_persistence_diagram(PD0[0])
+            plt.show()
+            gudhi.plot_persistence_diagram(PD0[-1])
+            plt.show()
         
-        gos = []
-        
-        for vine in res:
-            vine = np.array(vine)
-            # print(vine)
-            gos.append(go.Scatter3d(x=vine[:,0], y=vine[:,1], z=vine[:,2], marker=dict(
-                size=2,
-            ),
-            line=dict(
-                width=2
-            )))
-        
-        xs = np.linspace(0, mx, 10)
-        zs = np.linspace(0, 1, 10)
-        xss, zss = np.meshgrid(xs, zs)
-        gos.append(go.Surface(x=zss, y=xss, z=xss, colorscale=[[0, '#333'], [1, '#333']], opacity=0.1, showscale=False)) # x - y = 0: diag plane
-        
-        fig = go.Figure(data=gos)
-        
-        fig.update_layout(
-            width=800,
-            height=700,
-            scene=dict(
-              xaxis_title='T (homotopy)',
-              yaxis_title='Birth',
-              zaxis_title='Death'
-          )
-        )
-        
-        fig.show()
+        if verbose == 2 or verbose == 3:
+            gos = []
+            
+            for vine in res:
+                vine = np.array(vine)
+                # vine = np.minimum(np.array(vine), INT_MAX) # UNCOMMENT TO PLOT INF LINE
+                # print(vine)
+                gos.append(go.Scatter3d(x=vine[:,0], y=vine[:,1], z=vine[:,2], marker=dict(
+                    size=2,
+                ),
+                line=dict(
+                    width=2
+                )))
+            
+            xs = np.linspace(min(0, mn), mx, 10)
+            zs = np.linspace(np.min(ts), np.max(ts), 10)
+            xss, zss = np.meshgrid(xs, zs)
+            gos.append(go.Surface(x=zss, y=xss, z=xss, colorscale=[[0, '#333'], [1, '#333']], opacity=0.1, showscale=False)) # x - y = 0: diag plane
+            
+            fig = go.Figure(data=gos)
+            
+            fig.update_layout(
+                width=800,
+                height=700,
+                scene=dict(
+                xaxis_title='T (homotopy)',
+                yaxis_title='Birth',
+                zaxis_title='Death'
+            )
+            )
+            
+            fig.show()
 
     return res
 
-def vdist(vines, fD, fL): # diag weight func, length weight func
+def vdist(vines, fD=lambda _:1, fL=lambda _:1): # diag weight func, length weight func
     V = 0
+
+    for i in range(len(vines)):
+        vines[i] = np.minimum(np.array(vines[i]), INT_MAX)
+
     for vine in vines:
         vine = np.array(vine)
     
         v, L = 0, 0
         for i in range(1, len(vine)):
-            l = np.linalg.norm(vine[i][1:]-vine[i-1][1:])
+            l = np.linalg.norm(vine[i][1:]-vine[i-1][1:], ord=np.inf)
             dt = vine[i][0] - vine[i-1][0]
     
             mid = np.mean([vine[i][1:],vine[i-1][1:]], axis=0)
-            proj = np.mean(mid)*np.ones(2,)
-            D = np.linalg.norm(proj-mid)
+            D = (mid[1]-mid[0])/2
             
-            v += fD(D)*l*dt
+            v += fD(D)*l
             L += l*dt
     
         v *= fL(L)
